30f969f9dbe089766f75eafcfbbb2ebc55f8cbc4
Eniola Ebony <jeffebony@gmailcom>
2023-09-13 17:25:53 +0100

    updated the person mode

--- models/personModel.js
+++ models/personModel.js
@@ -5,12 +5,6 @@ const personSchema = new mongoose.Schema(
     name: {
       type: String,
       required: true,
-      validate: {
-        validator: function (v) {
-          return /^[a-zA-Z]+$/.test(v);
-        },
-        message: (props) => `${props.value} is not a valid name`,
-      },
     },
     age: {
       type: Number,
